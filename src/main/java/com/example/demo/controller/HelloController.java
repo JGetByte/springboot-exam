ff4f3f729ff9514f5f02c575bb3e17fa2b159a9d
piyawut.chi <piyawut.chi@ascendcorp.com>
2020-12-04 15:38:26 +0700

    undo revert

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -31,8 +31,10 @@ public class HelloController {
         User user = userService.findByEmail("pchiradejnunt@gmail.com");
         System.out.println("name" + user.getName());
         hello.put("name", user.getName());
+        // test 1
+
         hello.put("email", user.getEmail());
-        
+        // test 2
         return hello;
     }
 }
